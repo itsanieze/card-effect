3b8bc36a460e30dcb56e485b8d39eeb65bf3e482
Mehrdad Pashaei <mehrdad.pashaei1@gmail.com>
2023-06-25 16:09:46 +0430

    fix clear code

---
+++ src/Select.jsx
@@ -0,0 +1,20 @@
+import "./style.css"
+
+const Select = ({onColorChange}) => {
+
+    return (
+        <div>
+            <h4> Select Color!</h4>
+        <select className="select"  onChange={onColorChange}>
+            <option value="#f06d06"> FogDog :) </option>
+            <option value="Red"> Red </option>
+            <option value="Yellow"> Yellow </option>
+            <option value="Blue"> Blue </option>
+            <option value="Green"> Green </option>
+        </select>
+        </div>
+    );
+}
+
+
+export default Select;
